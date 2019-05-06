4865edaa545401ae591a8540bf7a7a72f3331b0d
Aurélien VERNAY <aurevernay@yahoo.fr>
2019-05-06 17:17:50 +0200

    Updates

---
+++ precache-manifest.64512a3dfa6f077790d6b2ac94ecce32.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154931931970449cfdeff12527e3166e",
+    "url": "/react-simon-game/index.html"
+  },
+  {
+    "revision": "ff3379611417bfa3fe9b",
+    "url": "/react-simon-game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e7a2f2e718d6a371edd8",
+    "url": "/react-simon-game/static/css/main.5c05d006.chunk.css"
+  },
+  {
+    "revision": "ff3379611417bfa3fe9b",
+    "url": "/react-simon-game/static/js/2.722d061f.chunk.js"
+  },
+  {
+    "revision": "e7a2f2e718d6a371edd8",
+    "url": "/react-simon-game/static/js/main.e7f54dfc.chunk.js"
+  },
+  {
+    "revision": "9850a664b2a0b0565055",
+    "url": "/react-simon-game/static/js/runtime~main.f8563ed7.js"
+  },
+  {
+    "revision": "b89ccd445b3b537abc07f0909c8aadd5",
+    "url": "/react-simon-game/static/media/digital_counter_7_italic.b89ccd44.ttf"
+  },
+  {
+    "revision": "44183143238b910788ad92db5497f006",
+    "url": "/react-simon-game/static/media/logo-mini.44183143.svg"
+  },
+  {
+    "revision": "bad82e982dd329aaea003dac11d91c4e",
+    "url": "/react-simon-game/static/media/logo.bad82e98.png"
+  }
+]);
