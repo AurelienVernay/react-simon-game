5f7baf801f01a0c2c9f34667f0ed70e3d0b0095c
Aurélien VERNAY <aurevernay@yahoo.fr>
2019-05-06 15:59:50 +0200

    deployed docs

---
+++ docs/precache-manifest.dd856da66c953224e341701fa3c790ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fba353f0d9a65d55bce04080477ad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3379611417bfa3fe9b",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "43a9bfb02a0a148133f4",
+    "url": "/static/css/main.30b0b0c1.chunk.css"
+  },
+  {
+    "revision": "ff3379611417bfa3fe9b",
+    "url": "/static/js/2.722d061f.chunk.js"
+  },
+  {
+    "revision": "43a9bfb02a0a148133f4",
+    "url": "/static/js/main.3866bc86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b89ccd445b3b537abc07f0909c8aadd5",
+    "url": "/static/media/digital_counter_7_italic.b89ccd44.ttf"
+  },
+  {
+    "revision": "44183143238b910788ad92db5497f006",
+    "url": "/static/media/logo-mini.44183143.svg"
+  },
+  {
+    "revision": "bad82e982dd329aaea003dac11d91c4e",
+    "url": "/static/media/logo.bad82e98.png"
+  }
+]);
